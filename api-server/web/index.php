74865b705132fb47db3b0cb1db11b9067325f5b0
Rafael <rafacla@users.noreply.github.com>
2017-11-11 09:21:33 -0200

    Configurando o servidor

--- api-server/web/index.php
+++ api-server/web/index.php
@@ -42,7 +42,7 @@ $app->before(function(Request $request, Application $app) use ($app, $db, $stora
 			$request = OAuth2\Request::createFromGlobals();
 			$response = new OAuth2\Response();
 			if (!$server->verifyResourceRequest($request,$response)) {
-				return new Response($response."..", 200);
+				return new Response($response."..", 403);
 			}
 			$tData = $server->getAccessTokenData(OAuth2\Request::createFromGlobals());
 			
