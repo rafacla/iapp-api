cef5225dc797f60c58b9ff05c3d28f23ee4ddfda
Rafael <rafacla@users.noreply.github.com>
2017-11-19 22:42:54 -0200

    removido _ dos headers

--- api-server/web/index.php
+++ api-server/web/index.php
@@ -268,9 +268,9 @@ $app->get('/users/{id}', function (Request $request, $id) use ($app, $db, $user)
 
 //Rota para verificar se um cliente existe
 $app->get('/cliente', function (Request $request) use ($app, $db, $user) {
-	$client_id = $db->escape_string($request->headers->get("client_id"));
-	$client_secret = $db->escape_string($request->headers->get("client_secret"));
-	if ($request->headers->get("client_id")!=null && $request->headers->get("client_secret")!=null) {
+	$client_id = $db->escape_string($request->headers->get("clientid"));
+	$client_secret = $db->escape_string($request->headers->get("clientsecret"));
+	if ($request->headers->get("clientid")!=null && $request->headers->get("clientsecret")!=null) {
 		$sql_s = "SELECT user_id, ativo FROM oauth_clients WHERE client_id = '".$client_id."' AND client_secret='".$client_secret."';";
 		$rows = $db ->select($sql_s);
 		if ($rows) {
