2e3b3a6faca5e65d973c0a6277d5bb995c5d7ae0
Rafael <rafacla@users.noreply.github.com>
2018-01-23 15:32:10 -0200

    retirado debug

--- api-server/web/index.php
+++ api-server/web/index.php
@@ -381,7 +381,7 @@ $app->post('/diario', function (Request $request) use ($app, $user, $db) {
 			return new Response(json_encode($resposta),201);
 		}
 		else
-			return new Response("Erro ao atualizar banco de dados ".$sql_i,400);
+			return new Response("Erro ao atualizar banco de dados ",400);
 	} else {
 		return new Response("Sintaxe de entrada inválida",400);
 	}
